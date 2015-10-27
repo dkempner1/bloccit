f74c0530b1d7a535760071e4ebe5fc6459008f39
dkempner1 <dkempner1@hotmail.com>
2015-10-27 11:10:50 -0600

    Topics Validation

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 require 'rails_helper'
 include RandomData
 
+
+
 RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
   it { should have_many(:posts) }
-  it { should have_many(:sponsored_posts) }
+  it { should validate_presence_of(:name)}
+  it { should validate_presence_of(:description)}
+  it { should validate_length_of(:name).is_at_least(5)}
+  it { should validate_length_of(:description).is_at_least(15)}
 
   context "attributes" do
     it "should respond to name" do
