fcc38a42e12db4d69f2b13d8df70230e1bc4acda
dkempner1 <dkempner1@hotmail.com>
2015-10-28 05:18:05 -0600

    sign in files added, modified views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   post 'users/confirm' => 'users#confirm'
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   get 'about' => 'welcome#about'
   root 'welcome#index'
 end
