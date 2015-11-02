e14a165c42ca4bac6edc1187b5c0fdca60d924e4
dkempner1 <dkempner1@hotmail.com>
2015-11-02 10:04:05 -0700

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
-
-
-
   resources :topics do
+# #34
     resources :posts, except: [:index]
-    resources :sponsored_posts, except: [:index]
   end
+
+  resources :posts, only: [] do
+# #5
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
-  post 'users/confirm' => 'users#confirm'
 
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
-  root 'welcome#index'
-end
+
+  root to: 'welcome#index'
+
+
+ end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,92 +1,67 @@
 include RandomData
 
+# Create Users
 5.times do
-   user = User.create!(
-   name:     RandomData.random_name,
-   email:    RandomData.random_email,
-   password: RandomData.random_sentence
-   )
- end
- users = User.all
+  user = User.create!(
+# #3
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
 
+# Create Topics
 15.times do
   Topic.create!(
-  name: RandomData.random_sentence,
-  description: RandomData.random_paragraph
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
   )
 end
 topics = Topic.all
 
-50.times do
-  Post.create!(
-    user: users.sample,
-    topic: topics.sample,
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
-  )
-end
-posts = Post.all
 
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
+ # Create Posts
+ 50.times do
+ # #1
+   Post.create!(
+     user: users.sample,
+     topic: topics.sample,
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ posts = Post.all
 
-50.times do
-  Advertisement.create!(
-  title: RandomData.random_sentence,
-  copy: RandomData.random_paragraph,
-  price: RandomData.random_number
-  )
-end
+ # Create Comments
+ # #3
+ 100.times do
+   Comment.create!(
+ # #4
+     user: users.sample,
+     post: posts.sample,
+     body: RandomData.random_paragraph
+   )
+ end
 
-50.times do
-  Question.create!(
-  title: RandomData.random_sentence,
-  body: RandomData.random_paragraph
-  )
-end
-
-50.times do
-SponsoredPost.create!(
-  topic:  topics.sample,
-  title:  RandomData.random_sentence,
-  body:   RandomData.random_paragraph,
-  price:  RandomData.random_number
-  )
-end
-sponsored_posts = SponsoredPost.all
-
-admin = User.create!(
- name:     'Admin User',
- email:    'admin@example.com',
- password: 'helloworld',
- role:     'admin'
-)
-
-# Create a member
-member = User.create!(
- name:     'Member User',
- email:    'member@example.com',
- password: 'helloworld'
-)
-
-admin = User.create!(
-   name:     'Moderator User',
-   email:    'moderator@hotmail.com',
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
    password: 'helloworld',
-   role:     'moderator'
+   role:     'admin'
+ )
+
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
  )
 
 
-
-puts "Seed finished"
-puts "#{User.count} users created"
-puts "#{Topic.count} topics created"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
-puts "#{Advertisement.count} advertisements created"
-puts "#{Question.count} questions created"
-puts "#{SponsoredPost.count} sponsored posts created"
+ puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Topic.count} topics created"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
