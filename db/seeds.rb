52bcfe5d8d59801e970e03d39472193a628f0066
dkempner1 <dkempner1@hotmail.com>
2015-11-03 04:21:43 -0700

    files cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,30 +34,7 @@ posts = Post.all
   )
 end
 
-50.times do
-  Advertisement.create!(
-  title: RandomData.random_sentence,
-  copy: RandomData.random_paragraph,
-  price: RandomData.random_number
-  )
-end
 
-50.times do
-  Question.create!(
-  title: RandomData.random_sentence,
-  body: RandomData.random_paragraph
-  )
-end
-
-50.times do
-SponsoredPost.create!(
-  topic:  topics.sample,
-  title:  RandomData.random_sentence,
-  body:   RandomData.random_paragraph,
-  price:  RandomData.random_number
-  )
-end
-sponsored_posts = SponsoredPost.all
 
 admin = User.create!(
  name:     'Admin User',
@@ -87,6 +64,3 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
-puts "#{Advertisement.count} advertisements created"
-puts "#{Question.count} questions created"
-puts "#{SponsoredPost.count} sponsored posts created"
