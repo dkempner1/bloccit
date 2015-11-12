58097837ae5c788c53f338690a232873568653f5
dkempner1 <dkempner1@hotmail.com>
2015-11-12 04:46:21 -0700

    added voting and rankings

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ topics = Topic.all
  # Create Posts
  50.times do
  # #1
-   post =Post.create!(
+   post = Post.create!(
      user: users.sample,
      topic: topics.sample,
      title:  RandomData.random_sentence,
