5e24df74f0066e5667fae3cef1fdc636e55583ca
dkempner1 <dkempner1@hotmail.com>
2015-11-17 07:58:23 -0700

    helpers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -7,4 +7,10 @@ module PostsHelper
   def user_is_moderator_for_post?(post)
     current_user && (current_user == post.user || current_user.moderator?)
   end
+
+  def render_title_for(post)
+    output = "<h1>#{post.title}<br><small>"
+    output += "submitted #{time_ago_in_words(post.created_at)} ago by #{post.user.name}</small></h1>"
+    output.html_safe
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,25 @@
 module UsersHelper
+  def render_posts_for(user)
+    if user.posts.any?
+      render(user.posts).html_safe
+    else
+      "<p>This user has not submitted any posts yet.</p>".html_safe
+    end
+  end
+
+  def render_comments_for(user)
+    if user.comments.any?
+      render(user.comments).html_safe
+    else
+      "<p>This user has not submitted any comments yet.</p>".html_safe
+    end
+  end
+
+  def render_favorite_posts_for(user)
+    if user.favorites.any?
+      render(user.favorites.map{|fav| fav.post}).html_safe
+    else
+      "<p>This user has not favorited any posts yet.</p>".html_safe
+    end
+  end
 end
